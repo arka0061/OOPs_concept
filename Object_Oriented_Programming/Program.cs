ea6a0b1b7c76a19e83ca554ac26c531ecd75bd82
Arka <arka.spectacular@gmail.com>
2021-07-30 08:35:35 +0530

    [Arka]Add:Json File

---
+++ Object_Oriented_Programming/Program.cs
@@ -0,0 +1,15 @@
+﻿using Object_Oriented_Programming.InventoryManagement;
+using System;
+
+namespace Object_Oriented_Programming
+{
+    class Program
+    {
+        const String Inventory_Json = @"E:\Bridglabz\Object_Oriented_Programming\InventoryManagement\Inventory.json"; //@ define path
+        static void Main(string[] args)
+        {
+            InventoryMain main = new InventoryMain();
+            main.DisplayData(Inventory_Json);
+        }
+    }
+}
